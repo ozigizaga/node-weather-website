2c51ac458765d81981db19e55d28d32b48ad0fd8
Ozigizaga <ozedinho@yahoo.com>
2022-04-27 08:51:00 -0400

    URI-encode address to Mapbox

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const geocode = (address, callback) => {
-    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1Ijoib3ppZ2l6YWdhIiwiYSI6ImNqdXBtb3RzYzBvaHY0ZHJ2M3pnbWRyb2QifQ.Vkr01QvxwnXa1k_fblnfxA&limit=1'
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=pk.eyJ1Ijoib3ppZ2l6YWdhIiwiYSI6ImNqdXBtb3RzYzBvaHY0ZHJ2M3pnbWRyb2QifQ.Vkr01QvxwnXa1k_fblnfxA&limit=1'
 
     request({ url, json: true }, (error, {body}) => {
         if (error) {
